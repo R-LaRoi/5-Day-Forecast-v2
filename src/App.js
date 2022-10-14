9efaff52cbfde20f941773c64e2a041f063d4207
CrystalQuest112 <114012059+CrystalQuest112@users.noreply.github.com>
2022-10-14 11:16:02 +0200

    Github Link added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="Weather"></div>;
+  return (
+    <div className="Weather">
+      <h1> All in divine order. It is well!</h1>
+      <a href="https://github.com/CrystalQuest112/weather-v2" target="_blank ">
+        Open source code,
+      </a>{" "}
+      by Rachel
+    </div>
+  );
 }
 
 export default App;
